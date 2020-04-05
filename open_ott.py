bbc9185e9fcc6c10a0077a267d3ddcc9a59182c9
nithintata <tatanithin007@gmail.com>
2020-04-05 20:01:00 +0530

    new file

---
+++ open_ott.py
@@ -0,0 +1,6 @@
+import webbrowser
+webbrowser.open_new("https://www.zee5.com/")
+webbrowser.open_new_tab("https://www.primevideo.com/")
+webbrowser.open_new_tab("https://www.netflix.com/in/")
+webbrowser.open_new_tab("https://www.aha.video/")
+webbrowser.open_new_tab("https://www.hotstar.com/in")
